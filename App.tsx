dde747e0f4482a245aee1a6e1943162290ae4178
Nikita Shevchik <shevchik.nikita@gmail.com>
2022-11-25 18:30:20 +0300

    finish work with text and buttons

--- App.tsx
+++ App.tsx
@@ -1,25 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState(null)
+
+  const handleButtonPress: VoidFunction = () => {
+    Alert.alert('Do you want to set Text', 'Set text upper?', [{text: 'Set', onPress: ()=>setText(()=> 'Setted from the button')}, {text: 'Del', onPress: ()=> setText(null)}])
+  }
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello</Text>
+      <Text numberOfLines={1} onPress={()=> Alert.alert('Lorem ipsum dolor sit amet consectetur adipisicing elit.')} >Lorem...</Text>
       <Button
         title="Press me"
         onPress={() => Alert.alert('Simple Button pressed')}
       />
+      {text? <Text>{text}</Text> : null}
       <Button
-        title="Press me"
+        title="Set Up Text"
         touchSoundDisabled
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-      <Button
-        title="Press me"
-        touchSoundDisabled
-        color="#f194ff"
-        onPress={() => Alert.alert('AAAAAAAAAAAAAAAAAAAA')}
+        color={'pink'}
+        onPress={handleButtonPress}
       />
       <Button
         title="Press me"
@@ -27,7 +29,7 @@ export default function App() {
         onPress={() => Alert.alert('Cannot press this one')}
       />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -35,7 +37,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  buttonYellow:{
+    backgroundColor: 'yellow',
+    color: '#000'
+  }
 });
