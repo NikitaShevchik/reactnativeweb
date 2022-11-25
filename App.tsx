a958b5bf89d4f1c7c38408e05a105def927ae648
Nikita Shevchik <shevchik.nikita@gmail.com>
2022-11-25 17:31:02 +0300

    start learn native react

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Button
+        title="Press me"
+        touchSoundDisabled
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
+      <Button
+        title="Press me"
+        touchSoundDisabled
+        color="#f194ff"
+        onPress={() => Alert.alert('AAAAAAAAAAAAAAAAAAAA')}
+      />
+      <Button
+        title="Press me"
+        disabled
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
       <StatusBar style="auto" />
     </View>
   );
